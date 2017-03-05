a94e601b702aa455fca50037995f505f4b0398c4
Fabian Okeke <fnokeke@gmail.com>
2017-03-05 10:05:27 -0500

    restart app at reboot

---
+++ app/src/main/java/io/smalldata/beehiveapp/main/StartMyServiceAtBootReceiver.java
@@ -0,0 +1,18 @@
+package io.smalldata.beehiveapp.main;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class StartMyServiceAtBootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Intent startServiceIntent = new Intent(context, RefreshService.class);
+            context.startService(startServiceIntent);
+        }
+
+    }
+
+}
