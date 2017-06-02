246fbba2d3913c45f7feb19d3a36b05270308e7b
Fabian Okeke <fnokeke@gmail.com>
2017-06-02 11:01:53 -0400

    now autoupdating only once per day at 3AM

--- app/src/main/java/io/smalldata/beehiveapp/main/AutoUpdateAlarm.java
+++ app/src/main/java/io/smalldata/beehiveapp/main/AutoUpdateAlarm.java
@@ -46,7 +46,7 @@ public class AutoUpdateAlarm extends BroadcastReceiver {
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent i = new Intent(context, AutoUpdateAlarm.class);
         PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
-        am.setRepeating(AlarmManager.RTC_WAKEUP, getMillisUntilTriggerTime(22), AlarmManager.INTERVAL_HALF_HOUR, pi);
+        am.setRepeating(AlarmManager.RTC_WAKEUP, getMillisUntilTriggerTime(3), AlarmManager.INTERVAL_DAY, pi);
 //        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 60 * 60 * 1000, 1000 * 60, pi); // FIXME: 5/31/17 remove debug code
     }
 
